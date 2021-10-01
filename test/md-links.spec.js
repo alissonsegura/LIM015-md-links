ac3f05d936c9795d97a0d591cf8e0ba5878715fb
alissonsegura <alissonicoleseguratarazona@gmail.com>
2021-10-01 01:30:00 -0500

    recursive function

--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,12 +1,73 @@
 // const mdLinks = require('../');
 
-const path = require('../app.js');
+const path = require('../file_system/index.js');
+// import { pathExists } from '../file_system/index.js';
+//destructuring
 
-describe('getPath', () => {
+describe('pathExists', () => {
   it('Should be a function', () => {
-    expect(typeof path.getPath).toBe('function');
+    expect(typeof path.pathExists).toBe('function');
   });
-  // it('Should return path', () => {
-  //   expect(path.getPath('/Users/alissonsegura/Desktop/LIM015-md-links/index.js')).toBe('/Users/alissonsegura/Desktop/LIM015-md-links/index.js');
-  // });
+
+  it('Should return true', () => {
+    expect(path.pathExists('/Users/alissonsegura/Desktop/LIM015-md-links/file_system/index.js')).toBeTruthy();
+  });
+
+  it('Should return false', () => {
+    expect(path.pathExists('/Users/alissonsegura/Desktop/LIM015-md-links/ind.js')).toBeFalsy();
+  });
+});
+
+describe('absolutePath', () => {
+  it('Should be a function', () => {
+    expect(typeof path.absolutePath).toBe('function');
+  })
+
+  // it('Should return absolute path', () => {
+  //   expect(absolutePath('file_system/index.js')).toBe('/Users/alissonsegura/Desktop/LIM015-md-links/file_system/index.js');
+  // })
+})
+
+describe('isDirectory', () => {
+  it('Should be a function', () => {
+    expect(typeof path.isDirectory).toBe('function');
+  })
+  it('Should return true for a directory', () => {
+    expect(path.isDirectory('file_system')).toBeTruthy();
+  });
+  it('Should return false for a file', () => {
+    expect(path.isDirectory('file_system/index.js')).toBeFalsy();
+  });
+})
+
+describe('verifyMdFile', () => {
+  it('Should be a function', () => {
+    expect(typeof path.verifyMdFile).toBe('function');
+  });
+
+  it('Should return true to .md', () => {
+    expect(path.verifyMdFile('file_system/Readme.md')).toBeTruthy();
+  });
+
+  it('Debería ser false para .md', () => {
+    expect(path.verifyMdFile('file_system/index.js')).toBeFalsy();
+  });
+});
+
+describe('readDirectory', () => {
+  it('Should be a function', () => {
+    expect(typeof path.readDirectory).toBe('function');
+  });
+  it('Should show all directories', () => {
+    expect(path.readDirectory('file_system')).toEqual(['data.txt', 'index.js', 'Readme.md']);
+  });
+});
+
+describe('readFile', () => {
+  it('Should be a function', () => {
+    expect(typeof path.readFile).toBe('function');
+  });
+  it('Should read file content', () => {
+    expect(path.readFile('file_system/data.txt')).toBe("Hello world")
+  })
 });
